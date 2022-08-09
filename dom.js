4106b32015a4a9099889c445aba522fe3d6f0442
Kinzaa Sheikh <75093626+KinzaaSheikh@users.noreply.github.com>
2022-08-09 15:32:35 +0500

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,22 @@
+//DOM Manipulation
+
+//to get element by ID
+
+const title = document.getElementById('main-heading');
+console.log(title);
+//console logging can be done in chrome browser
+
+
+//to get element by class name
+
+//const listItem = document.getElementsByClassName('list-items')
+//its important to use single-quotations
+
+//get elements by tag
+
+const listItem = document.getElementsByTagName('li');
+
+
+//query selector
+
+const container = document.querySelector('div')
